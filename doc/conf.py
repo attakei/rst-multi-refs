95996aa54f65d25374ab77cc23878b988622c106
Kazuya Takei <myself@attakei.net>
2024-02-23 03:04:04 +0900

    doc: Configure for API reference

--- doc/conf.py
+++ doc/conf.py
@@ -5,10 +5,28 @@ author = "Kazuya Takei"
 release = "0.0.0"
 
 # -- General configuration
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output
 html_theme = "alabaster"
 html_static_path = ["_static"]
+
+# -- Extension configuration
+# For sphinx.ext.intersphinx
+intersphinx_mapping = {
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
+}
+
+
+def setup(app):
+    app.add_object_type(
+        "confval",
+        "confval",
+        objname="configuration value",
+        indextemplate="pair: %s; configuration value",
+    )
--- doc/index.rst
+++ doc/index.rst
@@ -4,6 +4,14 @@ Home
 
 ``rst-multi-refs`` is referencing support library for Sphinx users.
 
+Contents
+========
+
+.. toctree::
+   :maxdepth: 1
+
+   api
+
 Indices and tables
 ==================
 
