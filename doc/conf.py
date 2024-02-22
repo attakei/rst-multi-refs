695ce8290e83a952b4f18f45161d98392a273ebc
Kazuya Takei <myself@attakei.net>
2024-02-23 02:56:57 +0900

    doc: Init documentation

---
+++ doc/conf.py
@@ -0,0 +1,14 @@
+# -- Project information
+project = "rst-multi-refs"
+copyright = "2024, Kazuya Takei"
+author = "Kazuya Takei"
+release = "0.0.0"
+
+# -- General configuration
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ doc/index.rst
@@ -0,0 +1,12 @@
+====
+Home
+====
+
+``rst-multi-refs`` is referencing support library for Sphinx users.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
