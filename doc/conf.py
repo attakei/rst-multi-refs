1103badc490cec6e66c467ef422d3e36c5e5a91a
Kazuya Takei <myself@attakei.net>
2024-02-24 01:24:40 +0900

    doc: Use furo

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
 
 # -- Extension configuration
